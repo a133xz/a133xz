82cabe892f41649bfb3f6afe8b8fe42b1d138685
a133xz <22479344+a133xz@users.noreply.github.com>
2020-06-16 23:41:10 +0100

    add: components 🛶

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer>
+    <hr />
+    potato, tomato
+    <div>
+      <a href="https://github.com/a133xz" rel="noopener noreferrer" target="_blank">github</a>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+  max-width: var(--main-width);
+  margin: 0 auto;
+  margin-top: 6em;
+  font-size: 0.8rem;
+}
+hr {
+  margin-bottom: 1em;
+}
+div {
+  display: inline-flex;
+  float: right;
+  a {
+    color: var(--brand-white);
+    &:first-child {
+      margin-right: 15px;
+    }
+  }
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <nav>
+      <g-link to="/">code</g-link>
+      <g-link to="/music">music</g-link>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  margin: 2em 0 4em;
+}
+nav {
+  a {
+    color: var(--brand-grey);
+    &.active--exact {
+      color: var(--brand-white);
+    }
+    &:not(:first-child) {
+      margin-left: 15px;
+    }
+  }
+}
+</style>
