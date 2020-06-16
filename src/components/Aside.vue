79096d1e0c021dfba4769eeffb37b040c7b6d120
a133xz <22479344+a133xz@users.noreply.github.com>
2020-06-16 23:36:26 +0100

    add: not for now ⏳

---
+++ src/components/Aside.vue
@@ -0,0 +1,31 @@
+<template>
+  <aside>
+    👨‍💻
+    <div></div>
+    <a href="https://github.com/a133xz" rel="noopener noreferrer" target="_blank">github</a>
+  </aside>
+</template>
+
+<style lang="scss" scoped>
+aside {
+  position: sticky;
+  width: 300px;
+  top: 290px;
+  transform: rotate(-90deg);
+  display: flex;
+  transition: all 0.8s;
+  div {
+    border-bottom: 1px solid var(--brand-white);
+    width: 65px;
+    margin: auto 45px;
+    padding: 2px;
+  }
+  a {
+    color: var(--brand-white);
+    font-size: 0.8rem;
+  }
+  @media screen and (max-width: 1100px) {
+    display: none;
+  }
+}
+</style>
