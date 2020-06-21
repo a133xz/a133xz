4fc5987ed3fe0bd45908105b2e897136226b5391
Antonio R <22479344+a133xz@users.noreply.github.com>
2020-06-21 16:20:33 +0100

    🙈🙈

--- src/main.js
+++ src/main.js
@@ -6,10 +6,16 @@ import "@/assets/scss/main.scss"
 
 import DefaultLayout from "@/layouts/Default.vue"
 
-export default function(Vue, { router, head, isClient }) {
+export default function (Vue, { head }) {
   // Set default layout as a global component
   Vue.component("Layout", DefaultLayout)
   // for now
   head.titleTemplate = ""
   head.bodyAttrs = { class: "custom-body-class" }
 }
+
+console.log("%c 💅💅", "font-size: 4.5em; font-weight: bolder;")
+console.log(
+  "%c grab the code here: https://github.com/a133xz/a133xz",
+  "color: blue; font-size: .8rem; font-weight: bolder;"
+)
